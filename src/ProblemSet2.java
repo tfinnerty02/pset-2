3d1ee1b969057f2de828392a4705043836d807fa
Thomas Finnerty (School) <School@Thomass-MacBook-Pro.local>
2019-10-01 21:53:51 -0400

    final final commit

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -1,17 +1,5 @@
-/**
- * Problem Set 2.
- *
- * It's time to put your skills to the test. This problem set focuses on prompting
- * the user for information, manipulating that information, and printing it in
- * specific formats.
- *
- * The specifications for each exercise are outlined below. Your job is to write
- * lines of code to produce answers to my questions. Each exercise requires that
- * you ask the user to enter one or more values. Your code must meet the
- * requirements set forth in this section (as well as the Deliverables section).
- *
- * Work through these exercises on your own. Experiment, make mistakes, ask
- * questions, and fix your mistakes. It's the only way to get good at programming.
+/*
+ * Copyright (c) 2019. UCVTS and its affiliated.
  */
 
 import java.util.Scanner;
@@ -23,13 +11,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 1.
-         *
-         * Prompt the user to enter the following information (in order): first name, last
-         * name, grade, age, and hometown.
-         *
-         * git add [filename]
-         * git commit -m "comment"
-         * git push origin master
          */
 
         System.out.print("\nEnter your first name: ");
@@ -50,9 +31,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 2.
-         *
-         * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
-         * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
 
         System.out.print("\nEnter a dollar amount: ");
@@ -82,9 +60,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 3.
-         *
-         * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
-         * bills and coins needed to produce this amount.
          */
 
         System.out.print("\nEnter a dollar amount: ");
@@ -123,9 +98,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 4.
-         *
-         * Given a number of inches, print the equivalent number of miles, yards, feet,
-         * and inches.
          */
 
         System.out.print("\nEnter a number of inches: ");
@@ -147,9 +119,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 5.
-         *
-         * Given a number of centimeters, print the equivalent number of kilometers,
-         * meters, and centimeters.
          */
 
         System.out.print("\nEnter a number of centimeters: ");
@@ -167,8 +136,6 @@ public class ProblemSet2 {
 
         /*
          * Exercise 6.
-         *
-         * Given a diameter, print the area and circumference of the corresponding circle.
          */
 
         System.out.print("\nEnter a diameter: ");
@@ -182,41 +149,33 @@ public class ProblemSet2 {
 
         /*
          * Exercise 7.
-         *
-         * Given a length and width, print the area, perimeter, and diagonal of the
-         * corresponding rectangle.
          */
 
         System.out.print("\nEnter a length: ");
         int length = in .nextInt();
         System.out.print("Enter a width: ");
         int width = in .nextInt();
-        double perimeter7 = (2*(double)length) + (2*(double)width);
+        double perimeter7 = (2 * (double) length) + (2 * (double) width);
 
-        System.out.printf("\nAREA      : %,.2f", (double)(length*width));
+        System.out.printf("\nAREA      : %,.2f", (double)(length * width));
         System.out.printf("\nPERIMETER : %,.2f", perimeter7);
         System.out.printf("\nDIAGONAL  : %,.2f\n", Math.hypot(length, width));
 
         /*
          * Exercise 8.
-         *
-         * Given a side length, print the area and perimeter of the corresponding hexagon.
          */
 
         System.out.print("\nEnter a side length: ");
-        int sideLength = in .nextInt();
-        in.nextLine();
+        int sideLength = in .nextInt(); in .nextLine();
 
         double hexArea = ((3 * Math.sqrt(3)) / 2) * Math.pow(sideLength, 2);
-        double hexPerim = 6*sideLength;
+        double hexPerim = 6 * sideLength;
 
         System.out.printf("\nAREA      : %,.2f\n", hexArea);
         System.out.printf("PERIMETER : %,.2f\n", hexPerim);
 
         /*
          * Exercise 9.
-         *
-         * Given a string, reverse and print the first and second halves of that string.
          */
 
         System.out.print("\nEnter a String: ");
@@ -227,11 +186,8 @@ public class ProblemSet2 {
 
         System.out.println("\n" + secondHalf + firstHalf);
 
-
         /*
          * Exercise 10.
-         *
-         * Given a first, middle, and last name, print the corresponding initials.
          */
 
         System.out.print("\nEnter your first name: ");
